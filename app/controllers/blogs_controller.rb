2c0ed37e495abe88091aba8e18ff79f95a630e9c
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-04-28 17:51:03 +0200

    integreates new, create and show actions for portfolio from scratch

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,25 +1,20 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy]
 
-  # GET /blogs
   def index
     @blogs = Blog.all
   end
 
-  # GET /blogs/1
   def show
   end
 
-  # GET /blogs/new
   def new
     @blog = Blog.new
   end
 
-  # GET /blogs/1/edit
   def edit
   end
 
-  # POST /blogs
   def create
     @blog = Blog.new(blog_params)
 
@@ -32,7 +27,6 @@ class BlogsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /blogs/1
   def update
     respond_to do |format|
       if @blog.update(blog_params)
@@ -43,7 +37,6 @@ class BlogsController < ApplicationController
     end
   end
 
-  # DELETE /blogs/1
   def destroy
     @blog.destroy
     respond_to do |format|
